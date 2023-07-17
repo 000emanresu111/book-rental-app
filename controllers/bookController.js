389808e2c12623cd3c250e47b592b382379a042a
Andrea Evangelista <andrea.evangelista@techedgegroup.com>
2023-07-17 15:48:52 +0200

    feat(controllers): edit rentBook to avoid race conditions using transactions and atomic db operations; implement test to check for race conditions; fix unit test for decreasing book quantity upon rental, stubbing the transaction

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -11,25 +11,40 @@ const getAllBooks = async (req, res, next) => {
 
 const rentBook = async (req, res, next) => {
   try {
-    const book = await Book.findById(req.params.id)
+    const bookId = req.params.id
+    const userTenantId = req.user.tenantId
 
-    if (!book) {
-      return res.status(404).json({ message: 'Book not found' })
+    // Using transactions and locks to prevent race conditions
+    const session = await Book.startSession()
+    session.startTransaction()
+
+    try {
+      // findOneAndUpdate() is atomic: the document can't change between when MongoDB finds the document and when MongoDB applies the update operation
+      const updatedBook = await Book.findOneAndUpdate(
+        { _id: bookId, bookstoreId: userTenantId, quantity: 1 },
+        { $inc: { quantity: -1 } },
+        { new: true, session }
+      )
+
+      if (!updatedBook) {
+        await session.abortTransaction()
+        session.endSession()
+        return res.status(404).json({ message: 'Book not found or out of stock' })
+      }
+
+      await session.commitTransaction()
+      session.endSession()
+
+      res.json(updatedBook)
+    } catch (error) {
+      await session.abortTransaction()
+      session.endSession()
+      next(error)
     }
-
-    if (book.quantity === 0) {
-      return res.status(400).json({ message: 'Book out of stock' })
-    }
-
-    book.quantity -= 1
-    await book.save()
-
-    res.json(book)
   } catch (error) {
     next(error)
   }
 }
-
 const returnBook = async (req, res, next) => {
   try {
     const book = await Book.findById(req.params.id)
--- tests/unitTests/bookController.test.js
+++ tests/unitTests/bookController.test.js
@@ -1,5 +1,6 @@
 const test = require('ava')
 const sinon = require('sinon')
+const { ObjectId } = require('mongoose').Types
 const { getAllBooks, rentBook, returnBook } = require('../../controllers/bookController')
 const Book = require('../../models/Book')
 
@@ -43,33 +44,93 @@ test('getAllBooks returns all books for a specific bookstore', async (t) => {
 })
 
 test('rentBook decreases the quantity of a book by 1', async (t) => {
-  const bookId = '1'
-  const book = { _id: bookId, title: 'Book 1', author: 'Author 1', quantity: 5, bookstoreId: '123', save: sinon.stub().resolves() }
+  const bookId = new ObjectId()
+  const userTenantId = 'userTenant1'
 
-  findByIdStub = sinon.stub(Book, 'findById')
-  findByIdStub.withArgs(bookId).resolves(book)
-
-  const req = {
-    params: {
-      id: bookId
-    }
+  const book = {
+    _id: bookId,
+    title: 'Book 1',
+    author: 'Author 1',
+    quantity: 5,
+    bookstoreId: userTenantId
   }
 
+  const findOneAndUpdateStub = sinon.stub(Book, 'findOneAndUpdate').resolves(book)
+  const startSessionStub = sinon.stub(Book, 'startSession').resolves({
+    startTransaction: sinon.stub(),
+    commitTransaction: sinon.stub(),
+    abortTransaction: sinon.stub(),
+    endSession: sinon.stub()
+  })
+
+  const req = {
+    params: { id: bookId },
+    user: { tenantId: userTenantId }
+  }
   const res = {
     json: sinon.spy()
   }
-
   const next = sinon.spy()
 
   await rentBook(req, res, next)
 
-  t.true(findByIdStub.calledOnceWithExactly(bookId))
-  t.true(book.save.calledOnce)
-  t.is(book.quantity, 4)
+  t.true(findOneAndUpdateStub.calledOnceWithExactly(
+    { _id: bookId, bookstoreId: userTenantId, quantity: 1 },
+    { $inc: { quantity: -1 } },
+    { new: true, session: sinon.match.any }
+  ))
   t.true(res.json.calledOnceWithExactly(book))
   t.false(next.called)
 
-  findByIdStub.restore()
+  findOneAndUpdateStub.restore()
+  startSessionStub.restore()
+})
+
+test('race conditions are handled when multiple users attempt to rent the last copy of a book simultaneously', async (t) => {
+  const bookId = new ObjectId()
+  const userTenantId = 'userTenant1'
+
+  const book = {
+    _id: bookId,
+    title: 'Book 1',
+    author: 'Author 1',
+    quantity: 1,
+    bookstoreId: userTenantId
+  }
+
+  const findOneAndUpdateStub = sinon.stub(Book, 'findOneAndUpdate')
+  findOneAndUpdateStub.onFirstCall().resolves(book) // Simulate the current user successfully renting the book
+  findOneAndUpdateStub.onSecondCall().resolves(null) // Simulate another user attempting to rent the book
+
+  const startSessionStub = sinon.stub(Book, 'startSession').resolves({
+    startTransaction: sinon.stub(),
+    commitTransaction: sinon.stub(),
+    abortTransaction: sinon.stub(),
+    endSession: sinon.stub()
+  })
+
+  const req = {
+    params: { id: bookId },
+    user: { tenantId: userTenantId }
+  }
+  const res = {
+    status: sinon.stub().returnsThis(),
+    json: sinon.spy()
+  }
+  const next = sinon.spy()
+
+  await Promise.all([
+    rentBook(req, res, next),
+    rentBook(req, res, next)
+  ])
+
+  t.true(findOneAndUpdateStub.calledTwice)
+  t.true(res.status.calledWith(404))
+  t.true(res.json.calledWith({ message: 'Book not found or out of stock' }))
+  t.false(next.called)
+
+  findOneAndUpdateStub.restore()
+  startSessionStub.restore()
 })
 
 test('returnBook should increase the quantity of a book by 1', async (t) => {
@@ -82,8 +143,6 @@ test('returnBook should increase the quantity of a book by 1', async (t) => {
     bookstoreId: '123',
     save: sinon.stub().resolves()
   }
-
-  findByIdStub.restore()
 
   findByIdStub = sinon.stub(Book, 'findById')
   findByIdStub.withArgs(bookId).resolves(book)
