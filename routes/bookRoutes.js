d075bde1d1c2b757e3915b75192c4eb1d08388e6
Andrea Evangelista <andrea.evangelista@techedgegroup.com>
2023-07-17 22:44:11 +0200

    feat(routes): add search route to book routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router()
 router.get('/', authenticateUser, getAllBooks)
 router.post('/:bookId/rent', authenticateUser, rentBook)
 router.post('/:bookId/return', authenticateUser, returnBook)
+router.get('/search', authenticateUser, searchBooks)
 
 module.exports = router
