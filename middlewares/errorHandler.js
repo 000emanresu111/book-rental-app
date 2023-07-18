faebcc8d000c3fa29d568115e04b6011bf364abf
Andrea Evangelista <andrea.evangelista@techedgegroup.com>
2023-07-18 22:32:39 +0200

    fix(middlewares): adapt erroHandler to be used with logger

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,9 +1,11 @@
+const { logger } = require('./logger')
+
 const errorHandler = (err, req, res, next) => {
   const status = err.status || 500
   const code = err.code || 'INTERNAL_SERVER_ERROR'
   const message = err.message || 'Internal Server Error'
 
-  console.error(err)
+  logger.error(err) // Log the error using the logger
 
   if (!res.headersSent) {
     const errorResponse = {
